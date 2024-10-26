46211aea85f6c9f2506d5c2d5dfbee0e436f8ec6
MintBrain <maximka602@gmail.com>
2024-10-26 15:28:55 +0500

    Add tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#41419D",
+        second: "#DBDEE5",
+        accentColor: "#4038CE",
+        text: "#000000",
+        textSecondary: "#34343B",
+        background: {primary: '#FFFFFF'}
+      },
+      fontFamily: {
+        default: 'Roboto, sans-serif',
+      },
+      fontSize: {
+        sm: ['14px', '20px'],
+        base: ['16px', '24px'],
+        lg: ['20px', '28px'],
+        xl: ['24px', '32px'],
+      }
+    },
+  },
+  plugins: [],
+};
