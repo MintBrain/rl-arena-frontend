ffd0a0c71fcff6a44a014651c75ecc525ca0e63e
MintBrain <maximka602@gmail.com>
2024-10-31 16:26:00 +0500

    Reformat code

--- src/Navigation/Navigation.tsx
+++ src/Navigation/Navigation.tsx
@@ -9,45 +9,27 @@ const { Sider } = Layout;
 
 type MenuItem = Required<MenuProps>["items"][number];
 
-function getItem(
-  label: React.ReactNode,
-  key: React.Key,
-  icon?: React.ReactNode,
-  children?: MenuItem[]
-): MenuItem {
-  return {
-    key,
-    icon,
-    children,
-    label
-  } as MenuItem;
+function getItem(label: React.ReactNode, key: React.Key, icon?: React.ReactNode, children?: MenuItem[]): MenuItem {
+  return { key, icon, children, label } as MenuItem;
 }
 
 const items: MenuItem[] = [
   getItem("Main page", "/", <PieChartOutlined />),
   getItem("Register", "/register", <DesktopOutlined />),
   getItem("Login", "/login", <DesktopOutlined />)
-  // getItem('User', 'sub1', <UserOutlined />, [
-  //   getItem('Tom', '4'),
-  //   getItem('Bill', '5'),
-  //   getItem('Alex', '6'),
-  // ]),
-  // getItem('Team', 'sub2', <TeamOutlined />, [getItem('Team 1', '7'), getItem('Team 2', '8')]),
-  // getItem('Files', '9', <FileOutlined />),
 ];
 
 const Navigation: React.FC = () => {
   const [collapsed, setCollapsed] = useState(false);
   const navigate = useNavigate();
 
-  function onMenuChange(data: SelectInfo) {
+  function handleMenuSelect(data: SelectInfo) {
     navigate(data.key);
   }
 
   return (
     <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
-      {/*<div className="demo-logo-vertical" />*/}
-      <Menu theme="dark" defaultSelectedKeys={["1"]} mode="inline" items={items} onSelect={onMenuChange} />
+      <Menu theme="dark" defaultSelectedKeys={["1"]} mode="inline" items={items} onSelect={handleMenuSelect} />
     </Sider>
   );
 };
